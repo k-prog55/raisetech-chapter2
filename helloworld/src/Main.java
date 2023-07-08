36176e199a30889f3d288faddf677cbe2be29874
k-prog55 <keianzai@keinoMacBook-Air.local>
2023-07-08 15:44:16 +0900

    .gitirnore作成

--- helloworld/src/Main.java
+++ helloworld/src/Main.java
@@ -2,5 +2,15 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("hey!");
+        }
+
+        for (int i = 0; i < 200; i++) {
+            System.out.println("Hello");
+        }
+
+
     }
 }
